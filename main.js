7b28358605c6d94d8cbe6025dc6c7ac9cafd9aa6
Roman Kovalchyk <kovalchykrv@gmail.com>
2024-01-27 08:12:46 +0200

    develop

---
+++ main.js
@@ -0,0 +1,28 @@
+
+import {r1, add} from "./reason.js";
+
+
+document.getElementById('d1').innerHTML = r1("нет света");
+
+let sum = add(7,5);
+
+function isVowel(char) {
+    return /^[ауоыэяюёие]$/.test(char.toLowerCase());
+}
+
+//document.getElementById('d2').innerHTML = sum;
+
+let input = prompt("Enter the name, please!");
+
+let lastLetter = input.substring(input.length-1);
+
+let isVow = isVowel(lastLetter);
+console.log(isVow);
+
+if(isVow){
+    input = input.substring(0, input.length-1);
+}
+let output = input + ", " + input + `всех по${input}ит`
+
+document.getElementById('d2').innerHTML = input;
+
